cad51c0310001bd84e657316f4dd95ea401e2cee
CliveMortimer <1020511@schoolsnet.act.edu.au>
2026-08-12 09:29:37 +1000

    p

--- _modules/_current/SNORE/src/sensitiveInformation.h
+++ _modules/_current/SNORE/src/sensitiveInformation.h
@@ -13,10 +13,10 @@ const char* ssid = "CyberRange";       // Wifi Network Name
 const char* password = "CyberRange";  // Wifi Password
 
 // MQTT client name
-const char* mqttClient = "CMREECM";
+const char* mqttClient = "SNORE";
 
 // MQTT Topic
-const char* mqttTopic = "challenges/CMREECM"; // It's worth noting that an ESP32 can subscribe to more than 1 topic
+const char* mqttTopic = "challenges/SNORE"; // It's worth noting that an ESP32 can subscribe to more than 1 topic
 
 // Replace with the MQTT broker IP address and port (default port for MQTT is 1883)
 const char* mqttServer = "192.168.1.116";  
